0eb131e6e94ffd209892038a77715d18ea2ac0b6
shyde <scott.hyde@panerabread.com>
2021-04-17 22:32:17 -0500

    add form input and styles

---
+++ src/Components/SignInAndSignUp/SignIn/SignIn.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import FormInput from '../FormInput/FormInput';
+
+class SignIn extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            password: '',
+        }
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        this.setState({email: '', password:''})
+    }
+
+    handleChange = event => {
+        const { value, name } = event.target;
+        this.setState({[name]: value}) // this will pull the name either password or email and dynamically set the sate property
+    }
+
+    render() {
+        return (
+            <div className='sign-in'>
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput
+                        name='email'
+                        type='email'
+                        value={this.state.email}
+                        handleChange={this.handleChange}
+                        label='email'
+                        required />
+                    <FormInput
+                        name='password'
+                        type='password'
+                        value={this.state.password}
+                        handleChange={this.handleChange}
+                        label='password'
+                        required />
+                    <input type="submit" value='Submit Form'></input>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignIn;
