9deb0ce22b5826f10b5e17e7b56581f5cacf714e
shyde <scott.hyde@panerabread.com>
2021-04-19 14:50:21 -0500

    add cart dropdown functionality with state

--- src/Components/Header/ShoppingCartIcon/ShoppingCartIcon.js
+++ src/Components/Header/ShoppingCartIcon/ShoppingCartIcon.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { toggleCartHidden } from '../../../redux/CartReducer/cartReducer';
 import './cart-icon.styles.scss';
 import { ReactComponent as ShoppingIcon } from '../../../Assets/shopping-bag.svg';
 
-const ShoppingCartIcon = () => (
-    <div className='cart-icon'>
+const ShoppingCartIcon = ({ toggleCartHidden }) => (
+    <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
         <span className='item-count'>0</span>
     </div>
 )
 
-export default ShoppingCartIcon;
+const mapDispatchToProps = dispatch => ({
+    toggleCartHidden: () => dispatch(toggleCartHidden()),
+})
+
+export default connect(null, mapDispatchToProps)(ShoppingCartIcon);
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import userReducer from './UserReducer/userReducer';
+import cartReducer from './CartReducer/cartReducer';
 
 export default combineReducers({
-    user: userReducer
+    user: userReducer,
+    cart: cartReducer,
 })
