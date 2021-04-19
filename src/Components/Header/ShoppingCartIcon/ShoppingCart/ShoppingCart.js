b6697c1f6499708484b0c460afec773483025488
shyde <scott.hyde@panerabread.com>
2021-04-19 16:04:03 -0500

    add cart item component and added reselect library. replaced cart selectors with memoized selectors

--- src/Components/Header/ShoppingCartIcon/ShoppingCart/ShoppingCart.js
+++ src/Components/Header/ShoppingCartIcon/ShoppingCart/ShoppingCart.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import  CustomButton from '../../../CustomButton/CustomButton';
+import CartItem from './CartItem/CartItem';
+import { selectCartItems } from '../../../../redux/CartReducer/cartReducer';
 import './cart-dropdown.styles.scss';
 
-const ShoppingCart = () => (
+const ShoppingCart = ({cartItems}) => (
     <div className='cart-dropdown'>
-        <div className='cart-items'/>
+        <div className='cart-items'>
+            {cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem}/>)}
+        </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 )
 
-export default ShoppingCart;
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state),
+})
+
+export default connect(mapStateToProps)(ShoppingCart);
