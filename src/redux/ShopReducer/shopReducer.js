b9879a25eba3e74ba2ee0ec2a317d5dd59f8efba
shyde <scott.hyde@panerabread.com>
2021-06-09 16:03:01 -0500

    add redux thunk and move shop data firebase call to shopReducer

--- src/redux/ShopReducer/shopReducer.js
+++ src/redux/ShopReducer/shopReducer.js
@@ -1,15 +1,45 @@
 import { createSelector } from 'reselect';
+import {convertCollectionsSnapshotToMap, firestore} from "../../firebase/firebase.utils";
 
 // Constants
-export const UPDATE_COLLECTIONS = 'UPDATE_COLLECTIONS';
+export const FETCH_COLLECTIONS_START = 'FETCH_COLLECTIONS_START';
+export const FETCH_COLLECTIONS_SUCCESS = 'FETCH_COLLECTIONS_SUCCESS';
+export const FETCH_COLLECTIONS_FAILURE = 'FETCH_COLLECTIONS_FAILURE';
 
 // Action Creators
-export function updateCollectionAction(collections) {
+export function fetchCollectionsStartAction() {
     return {
-        type: UPDATE_COLLECTIONS,
-        payload: collections
+        type: FETCH_COLLECTIONS_START,
     }
 }
+
+export function fetchCollectionsSuccessAction(collectionsMap) {
+    return {
+        type: FETCH_COLLECTIONS_SUCCESS,
+        payload: collectionsMap
+    }
+}
+
+export function fetchCollectionsFailureAction(errorMessage) {
+    return {
+        type: FETCH_COLLECTIONS_FAILURE,
+        payload: errorMessage,
+    }
+}
+
+export const fetchCollectionStartActionAsync = () => {
+    return dispatch => {
+        const collectionRef = firestore.collection('collections');
+        dispatch(fetchCollectionsStartAction()) // dispatch action saying the api call started
+        collectionRef.get()
+            .then(snapshot => {
+                const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+                dispatch(fetchCollectionsSuccessAction(collectionsMap))
+            })
+            .catch(error => dispatch(fetchCollectionsFailureAction(error)))
+    }
+}
+
 
 // Selectors
 // Input Selector
@@ -30,15 +60,26 @@ export function selectCollection(collectionUrlParam) {
     )
 }
 
+export const selectIsCollectionFetching = createSelector(
+    [selectShop],
+    shop => shop.isFetching
+)
+
 // Initial State
 const INITIAL_STATE = {
-    collections: null
+    collections: null,
+    isFetching: false,
+    errorMessage: undefined,
 }
 
 const shopReducer = (state = INITIAL_STATE, action) => {
     switch(action.type) {
-        case UPDATE_COLLECTIONS:
-            return { ...state, collections: action.payload }
+        case FETCH_COLLECTIONS_START:
+            return { ...state, isFetching: true }
+        case FETCH_COLLECTIONS_SUCCESS:
+            return { ...state, isFetching: false, collections: action.payload }
+        case FETCH_COLLECTIONS_FAILURE:
+            return { ...state, isFetching: false, errorMessage: action.payload }
         default:
             return state;
     }
